f2d3c90c1b1b8387dec94c90f140bb84ac21301f
Gustavo Costa <gucostarp@gmail.com>
2021-06-11 10:18:29 -0300

    Alterações para Deploy

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Controllers = require('../controller/Index');
+const Controllers = require('../controller/index');
 const authMiddleware = require('../Middleware/auth');
 
 
